6e592711710453a3787ceb8603adae53baa37d9f
Darwin <salinash2000@gmail.com>
2021-04-02 00:06:59 -0600

    showing modal empty modal form for adding and editing a task

--- resources/js/Todo/TodoItem.vue
+++ resources/js/Todo/TodoItem.vue
@@ -43,7 +43,22 @@
     </td>
 
     <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-      <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+      <a @click="edit" href="#" class="text-indigo-600 hover:text-indigo-900">
+        <svg
+          class="h-4 w-4 text-red-600"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+          />
+        </svg>
+      </a>
     </td>
   </tr>
 </template>
@@ -54,12 +69,17 @@ export default {
   },
   data() {
     return {
-        status: {
-            'Todo': 1,
-            'Doing': 2,
-            'Completed': 3,
-        }
+      status: {
+        Todo: 1,
+        Doing: 2,
+        Completed: 3,
+      },
     };
+  },
+  methods: {
+    edit() {
+      this.$emit("edit");
+    },
   },
 };
 </script>
