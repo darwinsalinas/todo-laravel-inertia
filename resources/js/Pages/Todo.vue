ae4e6e0f90246fd5db213f08731b9b2f7d0feabf
Darwin <salinash2000@gmail.com>
2021-04-01 22:57:18 -0600

    vue components for TodoList and TodoItem

---
+++ resources/js/Pages/Todo.vue
@@ -0,0 +1,31 @@
+
+<template>
+    <AppLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                TODO
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    <TodoList />
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
+
+<script>
+    import AppLayout from '@/Layouts/AppLayout.vue';
+    import TodoList from '@/Todo/TodoList.vue'
+
+    export default {
+        components: {
+            AppLayout,
+            TodoList
+        },
+        name: 'TodoPage'
+    }
+</script>
--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        // 'canLogin' => Route::has('login'),
-        'canLogin' => false,
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::middleware(['auth:sanctum', 'verified'])->get('/', function () {
+    return Inertia::render('Todo');
+})->name('home');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return Inertia::render('Dashboard');
